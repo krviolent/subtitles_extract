0f0e769e3b8da4e790a0d1054e33a6bb66bebdff
krviolent <rasimviolet@mail.ru>
2021-10-19 13:47:47 +0300

    Add files via upload

--- scritps/divide_timestamp_and_text.py
+++ scritps/divide_timestamp_and_text.py
@@ -1,17 +1,19 @@
-EP = 31
-import os
+import os, sys
 from os import listdir, getcwd, path
+
+EP = sys.argv[1]
+
 def listToString(s): 
     # initialize an empty string
     str1 = " "     
     # return string  
     return (str1.join(s))
 
-filename = f"subs_file_{EP}.txt"
 mypath = path.abspath(os.getcwd())
 timestamps = list()
 textonly = list()
 subs_dir = mypath + f"/subs/EP.A.{EP}/"
+filename = mypath + f"/subs/" + f"subs_file_{EP}.txt"
 print(subs_dir)
 if not os.path.isdir(subs_dir):
     os.mkdir(subs_dir)
@@ -24,6 +26,6 @@ for line in lines:
     #print(line)
     with open(subs_dir  + "timestamps.txt", "a+") as ti:
         ti.write(line[0] + "\n")
-        print(line[0])
+        #print(line[0])
     with open(subs_dir + "textonly.txt", "a+") as te:
-        te.write(listToString(line[1:]) + "\n")
+        te.write(listToString(line[1:]))
