82391e1e0afe37b03321f1b049dfa90fb3b5858b
krviolent <rasimviolet@mail.ru>
2021-10-20 19:14:45 +0300

    Update run_extract_subs.sh

--- scripts/run_extract_subs.sh
+++ scripts/run_extract_subs.sh
@@ -8,5 +8,5 @@ bash $PWD/scripts/crop.sh $1
 # $2 - frame rate
 bash $PWD/scripts/cut_to_frame.sh $1_video-cropped.mp4 1 frame_$2
 #rm $1_video-cropped.mp4
-python3 $PWD/scripts/easyocr_test.py $2 $3
+python3 $PWD/scripts/easyocr_test.py $2 $3 $4
 echo -e "Extracted subtitles saved in $PWD/frame_$2/subs/EP.A.$2"
